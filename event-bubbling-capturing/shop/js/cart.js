7c01f9c079caede129dab7b0a1497bd613a44b44
Andrey <lolkinandrey@yahoo.com>
2019-04-15 21:14:20 +0300

    event-bubbling-capturing homework

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const itemsList = document.querySelector('.items-list');
+
+function onAddToCartButtonClick(event) {
+  const target = event.target;
+  if(!target.classList.contains('add-to-cart')) {
+    return;
+  }
+
+  event.preventDefault();
+  addToCart({title: target.dataset.title, price: target.dataset.price});
+}
+
+function initCart() {
+  itemsList.addEventListener('click',onAddToCartButtonClick);
+}
+
+document.addEventListener('DOMContentLoaded',initCart);
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if(event.target.tagName.toLowerCase() !== 'th') {
+    return;
+  }
+
+  const target = event.target;
+  const name = target.dataset.propName;
+  target.dataset.dir === '1' ? target.dataset.dir = -1 : target.dataset.dir = 1;
+  document.querySelector('table').dataset.sortBy = name;
+  sortTable(name,target.dataset.dir);
 }
