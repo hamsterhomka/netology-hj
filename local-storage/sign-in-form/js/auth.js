a18b63cc806dcc90f8d15802c98a33a36ecd7e00
Andrey <lolkinandrey@yahoo.com>
2019-04-21 14:33:12 +0300

    JSON data sign in form

--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -9,10 +9,16 @@ function onSignInSubmit(event) {
   event.preventDefault();
   const signInUrl = 'https://neto-api.herokuapp.com/signin';
   const formData = new FormData(signInForm);
+  let user = {};
+  formData.forEach((value,key) => user[key] = value);
+  user = JSON.stringify(user);
 
   fetch(signInUrl,{
     method: 'POST',
-    body: formData
+    body: user,
+    headers: {
+      'Content-Type': 'application/json'
+    }
   })
     .then((res) => {
       if (200 <= res.status && res.status < 300) {
@@ -36,10 +42,16 @@ function onSignUpSubmit(event) {
   event.preventDefault();
   const signUpUrl = 'https://neto-api.herokuapp.com/signup';
   const formData = new FormData(signUpForm);
+  let user = {};
+  formData.forEach((value,key) => user[key] = value);
+  user = JSON.stringify(user);
 
   fetch(signUpUrl,{
     method: 'POST',
-    body: formData
+    body: user,
+    headers: {
+      'Content-Type': 'application/json'
+    }
   })
     .then((res) => {
       if (200 <= res.status && res.status < 300) {
